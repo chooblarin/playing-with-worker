cb18707464df70aa899663a0d8f702af8a4d1e07
chooblarin <choo.bla.rin@gmail.com>
2020-10-01 03:09:40 +0000

    Deploying to gh-pages from  @ 4d74cf9c819fde8bab2f12ac6944802142dc18cd 🚀

---
+++ precache-manifest.955c764a6ccb94e7ade8d78bfffbcf11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12019bb4930ae5789a063140895380fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b4d30d06bab2157f969",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af28974817407d05bd2a",
+    "url": "/static/js/2.57c4f019.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.57c4f019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4d30d06bab2157f969",
+    "url": "/static/js/main.dc6c5632.chunk.js"
+  },
+  {
+    "revision": "6b888b65635bb58e0e39",
+    "url": "/static/js/runtime-main.f538d2be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
